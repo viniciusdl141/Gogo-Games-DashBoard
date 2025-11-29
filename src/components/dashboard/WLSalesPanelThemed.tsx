4e3263818be9e924ef7d148f80c37f4641e97469
[dyad] <viniciusdl141@gmail.com>
2025-11-28 22:47:09 -0300

    [dyad] wrote 1 file(s)

--- src/components/dashboard/WLSalesPanelThemed.tsx
+++ src/components/dashboard/WLSalesPanelThemed.tsx
@@ -11,6 +11,7 @@ import WLSalesTablePanel from './WLSalesTablePanel';
 import ExportDataButton from './ExportDataButton';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import AddDailyWLSalesForm from './AddDailyWLSalesForm';
+import AddWLSalesForm from './AddWLSalesForm'; // <-- Adicionando a importação
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
