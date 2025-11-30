89006a8cc8b9f5a4aee7e8f2d34c86bc45d7a060
[dyad] <viniciusdl141@gmail.com>
2025-11-29 22:00:17 -0300

    [dyad] Configurando o TanStack Query Provider - wrote 3 file(s), added @tanstack/react-query package(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,27 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import IndexPage from './pages/Index';
 import ReferencesPage from './pages/References';
-import { SessionContextProvider } from './components/SessionContextProvider'; // Importando o provedor de sessão
+import { SessionContextProvider } from './components/SessionContextProvider';
+
+// Cria uma instância do QueryClient
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <SessionContextProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/references" element={<ReferencesPage />} />
-          {/* Adicione outras rotas aqui conforme necessário */}
-        </Routes>
-      </Router>
-    </SessionContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/references" element={<ReferencesPage />} />
+            {/* Adicione outras rotas aqui conforme necessário */}
+          </Routes>
+        </Router>
+      </SessionContextProvider>
+    </QueryClientProvider>
   );
 }
 
